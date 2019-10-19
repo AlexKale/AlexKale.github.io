f5b36e48970e9299a3fd0b3d56ca0ec77f0bb5e2
Alex Kale <alex-kale@github.com>
2019-10-20 01:30:29 +0300

    remove settimeout

--- js/index.js
+++ js/index.js
@@ -1,35 +1,33 @@
 document.addEventListener("DOMContentLoaded", function () {
   const buttonsContainer = document.querySelector(".buttons-wrap");
   const fridgeInfoContainer = document.querySelector(".card_size_m:nth-child(8) .card-description");
-  setTimeout(function() {
-      const confirmPurchaseButton = document.querySelector(".buttons-wrap .button_yellow");
-      const purchaseListContainer = document.createElement('div');
-      const purchaseListTitle = document.createElement('p');
-      const purchaseList = document.createElement('ol');
-      const purchaseListItemOne = document.createElement('li');
-      const purchaseListItemTwo = document.createElement('li');
+  const confirmPurchaseButton = document.querySelector(".buttons-wrap .button_yellow");
 
-      purchaseListContainer.setAttribute('class', 'purchase-list-wrap');
-      purchaseListTitle.setAttribute('class', 'card-description card-description_big description_critical');
-      purchaseListTitle.textContent = 'Список покупок:';
-      purchaseList.setAttribute('class', 'purchase-list');
-      purchaseListItemOne.setAttribute('class', 'purchase-list__item');
-      purchaseListItemOne.textContent = 'Хлеб';
-      purchaseListItemTwo.setAttribute('class', 'purchase-list__item');
-      purchaseListItemTwo.textContent = 'Молоко';
+  const purchaseListContainer = document.createElement('div');
+  const purchaseListTitle = document.createElement('p');
+  const purchaseList = document.createElement('ol');
+  const purchaseListItemOne = document.createElement('li');
+  const purchaseListItemTwo = document.createElement('li');
 
-      purchaseListContainer.appendChild(purchaseListTitle);
-      purchaseListContainer.appendChild(purchaseList);
-      purchaseList.appendChild(purchaseListItemOne);
-      purchaseList.appendChild(purchaseListItemTwo);
+  purchaseListContainer.setAttribute('class', 'purchase-list-wrap');
+  purchaseListTitle.setAttribute('class', 'card-description card-description_big description_critical');
+  purchaseListTitle.textContent = 'Список покупок:';
+  purchaseList.setAttribute('class', 'purchase-list');
+  purchaseListItemOne.setAttribute('class', 'purchase-list__item');
+  purchaseListItemOne.textContent = 'Хлеб';
+  purchaseListItemTwo.setAttribute('class', 'purchase-list__item');
+  purchaseListItemTwo.textContent = 'Молоко';
 
-      confirmPurchaseButton.onclick = () => {
-          fridgeInfoContainer.replaceWith(purchaseListContainer)
-          buttonsContainer.style.display = "none";
-      }
-  }, 500)
+  purchaseListContainer.appendChild(purchaseListTitle);
+  purchaseListContainer.appendChild(purchaseList);
+  purchaseList.appendChild(purchaseListItemOne);
+  purchaseList.appendChild(purchaseListItemTwo);
+
+  confirmPurchaseButton.onclick = () => {
+      fridgeInfoContainer.replaceWith(purchaseListContainer)
+      buttonsContainer.style.display = "none";
+  }
   
-
   document.querySelector(".header-menu__switcher").addEventListener("click", function () {
       document.querySelector(".header-menu").classList.toggle("header-menu_active")
   })
