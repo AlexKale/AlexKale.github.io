f30553d2c4c86d3823dbe30947e542af58ac72d6
Alex Kale <alex-kale@github.com>
2019-10-19 23:49:30 +0300

    update scripts

--- js/main.js
+++ js/main.js
@@ -24453,17 +24453,6 @@ const devices = document.querySelector('.devices');
 const pagiantorDevs = document.querySelector('.devices__paginator');
 let currentPageDevs = 1;
 
-// pagiantorDevs.classList.toggle('paginator_hide', panelCountDevs < 7);
-
-$('.card').each(function(e) {
-    if ($(this).hasClass('card_size_s')) {
-        $(this).css({'border-radius': '22px'})
-    } else {
-        $(this).css({'border-radius': '54px'})
-    }
-});
-
-
 let curValue;
 let curRotate;
 let maxRotate = 0.42; // 150 градусов
@@ -24613,22 +24602,7 @@ document.querySelectorAll('.panel_floor').forEach(p => {
     }
 });
 
-document.addEventListener("DOMContentLoaded", function () {
-    var waterContainer = document.querySelector('.card.card_size_s:last-child');
 
-    waterContainer.innerHTML = 
-                '<div class="card-heading">' +
-                    '<div class="card-icon-wrap">' +
-                        '<img class="card-icon" src="img/kettle.svg">' +
-                    '</div>' +
-                    '<h3 class="card-title">Вода вскипела</h3>' +
-               ' </div>' +
-                '<div class="card-specs">' +
-                    '<p class="card-source">Чайник</p>' +
-                    '<p class="card-time card-time_block">16:20, Сегодня</p>' +
-                '</div>'
-
-});
 
 
 const arrowLeftScens = document.querySelector('.scenarios__paginator .paginator__arrow_left');
@@ -24721,40 +24695,7 @@ function buildHTML(e) {
 }
 
 document.addEventListener("DOMContentLoaded", function () {
-    const buttonsContainer = document.querySelector(".buttons-wrap");
-    const fridgeInfoContainer = document.querySelector(".card_size_m:nth-child(8) .card-description");
-    setTimeout(function() {
-        const confirmPurchaseButton = document.querySelector(".buttons-wrap .button_yellow");
-        const purchaseListContainer = document.createElement('div');
-        const purchaseListTitle = document.createElement('p');
-        const purchaseList = document.createElement('ol');
-        const purchaseListItemOne = document.createElement('li');
-        const purchaseListItemTwo = document.createElement('li');
-
-        purchaseListContainer.setAttribute('class', 'purchase-list-wrap');
-        purchaseListTitle.setAttribute('class', 'card-description card-description_big description_critical');
-        purchaseListTitle.textContent = 'Список покупок:';
-        purchaseList.setAttribute('class', 'purchase-list');
-        purchaseListItemOne.setAttribute('class', 'purchase-list__item');
-        purchaseListItemOne.textContent = 'Хлеб';
-        purchaseListItemTwo.setAttribute('class', 'purchase-list__item');
-        purchaseListItemTwo.textContent = 'Молоко';
-
-        purchaseListContainer.appendChild(purchaseListTitle);
-        purchaseListContainer.appendChild(purchaseList);
-        purchaseList.appendChild(purchaseListItemOne);
-        purchaseList.appendChild(purchaseListItemTwo);
-
-        confirmPurchaseButton.onclick = () => {
-            fridgeInfoContainer.replaceWith(purchaseListContainer)
-            buttonsContainer.style.display = "none";
-        }
-    }, 500)
-   
-
-    document.getElementsByClassName("header-menu__switcher")[0].addEventListener("click", function () {
-        document.getElementsByClassName("header-menu")[0].classList.toggle("header-menu_active")
-    })
+    
 }, !1);
 var initVideoObs = function () {
     var t = 100, n = 100;
